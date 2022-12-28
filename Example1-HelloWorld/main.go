e34190b88324295138756a86ddb7dde850c1d05e
Sudhir Tataraju <stataraju@salesforce.com>
2022-12-28 11:15:01 +0530

    My first Go program

---
+++ Example1-HelloWorld/main.go
@@ -0,0 +1,13 @@
+package main 
+/* 
+Here "main" is the file name i.e. main.go
+main package declaration is must in a file that has to executable.
+A package is a shared library i.e. collection of source files in the same directory that are compiled together.
+*/
+
+import "fmt" //a module is a collection of packages with built-in dependencies and versioning.
+//note the double quotes.
+
+func main(){   //main function is the first function that executes in the program, here this declaration is must in a file that has to executable.
+	fmt.Println("Hello World")
+}
